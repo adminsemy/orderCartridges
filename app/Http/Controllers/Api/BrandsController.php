07705c2939dbf960218ea4828e0bd6c79eee3bca
Antonov.DA <den_ru1982@mail.ru>
2020-08-05 14:50:50 +0300

    Add return type for methods

--- app/Http/Controllers/Api/BrandsController.php
+++ app/Http/Controllers/Api/BrandsController.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\BrandsResource;
 use App\Http\Resources\Admin\PrinterNameResource;
 use App\Model\PrinterNames;
-use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class BrandsController extends Controller
 {
-    public function brands()
+    public function brands(): AnonymousResourceCollection
     {
         return PrinterNameResource::collection(PrinterNames::all());
     }
 
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         $brands = PrinterNames::all();
         return BrandsResource::collection($brands);
