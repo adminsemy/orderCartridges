47927e6b8bb84f543a21182a7cb4171875654aac
Antonov.DA <den_ru1982@mail.ru>
2020-08-05 22:45:21 +0300

    Add PHPDoc blocks

--- app/Http/Controllers/Api/BrandsController.php
+++ app/Http/Controllers/Api/BrandsController.php
@@ -9,12 +9,22 @@ use App\Model\PrinterNames;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class BrandsController extends Controller
-{
+{    
+    /**
+     * Return collection name printers
+     *
+     * @return AnonymousResourceCollection
+     */
     public function brands(): AnonymousResourceCollection
     {
         return PrinterNameResource::collection(PrinterNames::all());
     }
-
+    
+    /**
+     * Return collection printers with keys - id, name
+     *
+     * @return AnonymousResourceCollection
+     */
     public function index(): AnonymousResourceCollection
     {
         $brands = PrinterNames::all();
